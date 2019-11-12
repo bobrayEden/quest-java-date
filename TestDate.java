d1e8af58549c7aa18c669a322f1fafe27f10bca2
0_Nocaudie <o.nocaudie@googlemail.com>
2019-11-12 10:34:45 +0100

    Challenge Done, et le jour est le vrai jour

--- TestDate.java
+++ TestDate.java
@@ -1,8 +1,6 @@
+import java.sql.SQLOutput;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.Date;
-import java.util.Scanner; 
+import java.util.*;
 
 class TestDate {
 
@@ -18,6 +16,12 @@ class TestDate {
         sc.close();
 
         // TODO : your code after this line
-        
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
+        Calendar c = new GregorianCalendar(year, month - 1, day);
+        Date d = c.getTime();
+        System.out.println("The date is : " + sdf.format(d));
+        int dayOfWeek = c.get(Calendar.DAY_OF_WEEK) - 1;
+        if (dayOfWeek < 1) {dayOfWeek = 7;}
+        System.out.println("The day of week is : " + dayOfWeek);
     }
 }
